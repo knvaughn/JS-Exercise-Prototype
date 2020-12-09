a5c96921105dcf347a0ce5b7cd595358a19cb76b
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2020-12-08 21:15:08 -0500

    finished task 4

--- index.js
+++ index.js
@@ -39,15 +39,20 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
- 
-
-  
-  
-  
+ Person.prototype.eat = function(someFood) {
+   this.stomach.length < 10 ? this.stomach.push(someFood) : "";
+ }
+ Person.prototype.poop = function() {
+   this.stomach = [];
+ }
+ Person.prototype.toString = function() {
+   return `${this.name}, ${this.age}`;
+ }
   
   /*
     TASK 2
@@ -63,10 +68,19 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
-  
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
+  }
+  Car.prototype.drive = function(distance) {
+    this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
+  }
   
   /*
     TASK 3
@@ -75,18 +89,22 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy;
   }
- 
+ Baby.prototype = Object.create(Person.prototype);
+ Baby.prototype.play = function() {
+   return `Playing with ${this.favoriteToy}`;
+ }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Global - when this is not used in an object, it refers to the global window object
+    2. Implicit - When this is used inside of an object, it refers to that parent object
+    3. Explicit - when you choose which object you want this to refer to using call, apply, or bind
+    4. New - When you create a new object using the new keyword, this refers to that object
   */
   
   
